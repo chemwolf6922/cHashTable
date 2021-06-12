21ad2f2b020b5c34e02c415ac092b0ca8c6ee7ac
chemwolf6922 <wang6922@126.com>
2021-06-12 15:01:28 +0800

    add static hash table defines

---
+++ staticHashTable.c
@@ -0,0 +1,17 @@
+#include "staticHashTable.h"
+#include <malloc.h>
+
+typedef struct{
+    
+}static_hash_table_t;
+
+int static_hash_table_create(static_hash_table_handle_t* handle,hash_table_item* items,size_t len)
+{
+    *handle = NULL;
+    return -1;
+}
+
+void* static_hash_table_get(static_hash_table_handle_t* handle,uint8_t* key,size_t key_len)
+{
+    return NULL;
+}
---
+++ staticHashTable.h
@@ -0,0 +1,20 @@
+#ifndef __STATIC_HASH_TABLE_H
+#define __STATIC_HASH_TABLE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+/* This hash table is created once and does not accept new items */
+
+typedef void* static_hash_table_handle_t;
+
+typedef struct{
+    uint8_t* key;
+    size_t key_len;
+    void* data;
+}hash_table_item;
+
+int static_hash_table_create(static_hash_table_handle_t* handle,hash_table_item* items,size_t len);
+void* static_hash_table_get(static_hash_table_handle_t* handle,uint8_t* key,size_t key_len);
+
+#endif
